ea2f6b4cba0761232dd36dd5306a4c50ca8194f3
raven99p <49030609+raven99p@users.noreply.github.com>
2021-01-15 23:49:36 +0200

    Add files via upload

--- server/queries.js
+++ server/queries.js
@@ -1,90 +1,101 @@
-const Pool = require('pg').Pool
+const {Pool,Client} = require('pg');
 const pool = new Pool({
   user: 'me',
   host: 'localhost',
   database: 'api',
   password: 'password',
   port: 5432,
-})
+});
 
 
-const getEmployees = (request, response) => {
-  pool.query('SELECT * FROM Employee ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
+const getEmployees = async (request, response) => {
+    const client = await pool.connect();
+    try{
+      const results = await client.query('SELECT * FROM Employee ORDER BY id ASC');
+      response.status(200).json(results.rows);
+    }catch(e){
+      console.log(e);
+    }finally{
+      client.release();
     }
-    response.status(200).json(results.rows)
-  })
+  }
+
+const getEmployeeById = async (request, response) => {
+  const id = parseInt(request.params.id);
+  const client = await pool.connect();
+  try{
+    
+    const results = await client.query('SELECT * FROM Employee WHERE id = $1',[id]);
+    response.status(200).json(results.rows);
+  }catch(e){
+    console.log(e);
+  }finally{
+    client.release();
+  }
+
 }
 
-const getEmployeeById = (request, response) => {
-  const id = parseInt(request.params.id)
-
-  pool.query('SELECT * FROM Employee WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
 
 
-const createEmployee = (request, response) => {
+const createEmployee = async (request, response) => {
   const { first_name , last_name , is_active , date_of_birth } = request.body
-
-  pool.query('INSERT INTO Employee (first_name ,last_name , is_active , date_of_birth ) VALUES ($1,$2,$3,$4)', [first_name , last_name , is_active , date_of_birth], (error, results) => {
-    if (error) {
-      throw error
-    }
+  const client = await pool.connect();
+  try{
+    
+    const results = await client.query('INSERT INTO Employee (first_name ,last_name , is_active , date_of_birth ) VALUES ($1,$2,$3,$4)', [first_name , last_name , is_active , date_of_birth]);
     response.status(201).send(`Employee added with ID: ${results.insertId}`)
-  })
+  }catch(e){
+    console.log(e)
+  }finally{
+    client.release();
+  }
 }
 
 
-const updateEmployee = (request, response) => {
+
+const updateEmployee = async (request, response) => {
   const id = parseInt(request.params.id)
   const { first_name , last_name , is_active , date_of_birth } = request.body
-
-  pool.query(
-    'UPDATE Employee SET first_name = $1, last_name = $2, is_active =$3, date_of_birth = $4 WHERE id = $5',
-    [first_name , last_name , is_active , date_of_birth, id],
-    (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`Employee modified with ID: ${id}`)
-    }
-  )
+  const client = await pool.connect();
+  try{
+    
+    const results = await client.query('UPDATE Employee SET first_name = $1, last_name = $2, is_active =$3, date_of_birth = $4 WHERE id = $5', [first_name , last_name , is_active , date_of_birth, id]);
+    response.status(200).send(`Employee modified with ID: ${id}`);
+  }catch(e){
+    console.log(e)
+  }finally{
+    client.release();
+  }
 }
 
 
-const deleteEmployee = (request, response) => {
+const deleteEmployee = async (request, response) => {
   const id = parseInt(request.params.id)
-
-  pool.query('DELETE FROM Employee WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
+  const client = await pool.connect();
+  try{
+    
+    const results = await client.query('DELETE FROM Employee WHERE id = $1', [id])
     response.status(200).send(`Employee deleted with ID: ${id}`)
-  })
+  }catch(e){
+    console.log(e)
+  }finally{
+    client.release();
+  }
 }
 
 
-const deleteAllEmployees = (request, response) => {
-  pool.query('DELETE FROM Employee', (error, results) => {
-    if (error) {
-      throw error
-    }
+const deleteAllEmployees = async (request, response) => {
+  const client = await pool.connect();
+  try{
+    
+    const results = await client.query('DELETE FROM Employee')
     response.status(200).send(`Table cleared`)
-  })
+  }catch(e){
+    console.log(e)
+    }finally{
+      client.release();
+    }
 }
-
-
-
-
-
-
-
 
 
 
