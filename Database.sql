215a5a53015ac14b2a777b4a7a6184b7e2f55b2d
raven99p <49030609+raven99p@users.noreply.github.com>
2021-01-15 14:20:37 +0200

    Delete Database.sql

--- Database.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS Employee;
-CREATE TABLE Employee(
-	id bigserial PRIMARY KEY NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	first_name VARCHAR(50) NOT NULL,
-	is_active BOOLEAN NOT NULL,
-	date_of_birth DATE NOT NULL
-);
